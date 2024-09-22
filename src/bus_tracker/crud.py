32fe0d8afe68117e8ce8e8114c2bf74d1d39fed2
taharallouche <tahar.allouche.pro@gmail.com>
2024-09-22 18:14:55 +0200

    feat: add validation on log add

--- src/bus_tracker/crud.py
+++ src/bus_tracker/crud.py
@@ -1,14 +1,15 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 import bus_tracker.models as models
 from bus_tracker import schemas
 
 
-def get_bus(db: Session, line_number: int) -> models.Bus:
+def get_bus(db: Session, line_number: int) -> models.Bus | None:
     return db.query(models.Bus).filter(models.Bus.line_number == line_number).first()
 
 
-def get_buses(db: Session, skip: int = 0, limit: int = 100):
+def get_buses(db: Session, skip: int = 0, limit: int = 100) -> list[models.Bus]:
     return db.query(models.Bus).offset(skip).limit(limit).all()
 
 
@@ -20,15 +21,17 @@ def create_bus(db: Session, bus: schemas.BusCreate) -> models.Bus:
     return bus
 
 
-def get_log(db: Session, log_id: int) -> models.Log:
+def get_log(db: Session, log_id: int) -> models.Log | None:
     return db.query(models.Log).filter(models.Log.id == log_id).first()
 
 
-def get_logs(db: Session, skip: int = 0, limit: int = 100):
+def get_logs(db: Session, skip: int = 0, limit: int = 100) -> list[models.Log]:
     return db.query(models.Log).offset(skip).limit(limit).all()
 
 
-def get_logs_by_bus(db: Session, line_number: int, skip: int = 0, limit: int = 100):
+def get_logs_by_bus(
+    db: Session, line_number: int, skip: int = 0, limit: int = 100
+) -> list[models.Log]:
     return (
         db.query(models.Log)
         .filter(models.Log.line_number == line_number)
@@ -40,6 +43,10 @@ def get_logs_by_bus(db: Session, line_number: int, skip: int = 0, limit: int = 1
 
 
 def create_log(db: Session, log: schemas.LogCreate) -> models.Log:
+    bus = get_bus(db, log.line_number)
+    if not bus:
+        raise HTTPException(status_code=404, detail=f"Bus {log.line_number} not found")
+
     db_log = models.Log(**log.model_dump())
     db.add(db_log)
     db.commit()
